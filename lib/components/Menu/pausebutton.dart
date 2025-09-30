5cfd0deb6819002aa5157290b0b94c95c74180aa
gsb-b60 <dihieu2407@gmail.com>
2025-09-30 12:21:30 +0700

    main menu and pause menu ui

---
+++ lib/components/Menu/pausebutton.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:mygame/main.dart';
+
+class PauseButton extends StatelessWidget {
+  final MyGame game;
+  const PauseButton({super.key, required this.game});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 20,
+      right: 20,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.zero, // bỏ padding mặc định
+          backgroundColor: Colors.transparent, // nền trong suốt
+          shadowColor: Colors.transparent,
+        ),
+        onPressed: () {
+          game.pauseEngine();
+          game.overlays.add('PauseMenu'); // show menu pause
+          game.overlays.remove('PauseButton'); // ẩn nút khi menu hiện
+        },
+        child: Container(
+          height: 45,
+          child: Image.asset("assets/menu/pauseMenu/pauseBtn.png")),
+      ),
+    );
+  }
+}
